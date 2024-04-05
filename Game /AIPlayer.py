4a6e317d9ceadca115b31c42aa5f62c99c29755b
am22460 <tminah1307@gmail.com>
2024-04-05 17:32:54 +0100

    Added TreeNodes to AIPlayer

--- Game /AIPlayer.py
+++ Game /AIPlayer.py
@@ -1,4 +1,12 @@
 import math as math
+
+class TreeNode:
+    def __init__(self, state):
+        self.state = state
+        self.visits = 0
+        self.score = 0
+        self.children = {}
+        
 class AIPlayer:
     def __init__(self):
         self.root = None
