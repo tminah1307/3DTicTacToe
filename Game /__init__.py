cafa4470e4e0db5ba70d196504f854bfdd4d29c2
am22460 <tminah1307@gmail.com>
2024-04-06 16:24:56 +0100

    Reformatted __init__ for clarity

--- Game /__init__.py
+++ Game /__init__.py
@@ -1,6 +1,9 @@
+PACKAGE_VERSION = '1.0'
+
+__all__ = ['GameState', 'Board', 'Point', 'PointsAndScores', 'AIPlayer', 'TreeNode']
+
 from .GameState import GameState
-from .Board import Board
-from .Board import Point
-from .Board import PointsAndScores
-from .AIPlayer import AIPlayer
-from .AIPlayer import TreeNode
+from .Board import Board, Point, PointsAndScores
+from .AIPlayer import AIPlayer, TreeNode
+
+
